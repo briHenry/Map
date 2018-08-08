02f8e178775ed05b5f3c935b93941f5ca7a62bde
briHenry <bhenry58111@gmail.com>
2018-08-08 09:16:53 -0400

    Adding map and buttons

--- map.js
+++ map.js
@@ -28,9 +28,47 @@ function init() {
     });
 }
 
-// How do we know our web page loads for the first time?
-document.addEventListener("DOMContentLoaded", function (e){
-    console.log("LOADED LSKDMFLKSDMFLKSDMFLKM");
-    init();
-});
+// // How do we know our web page loads for the first time?
+// document.addEventListener("DOMContentLoaded", function (e){
+//     console.log("LOADED LSKDMFLKSDMFLKSDMFLKM");
+//     init();
+// });
+// the next line shows an easier way of writing this code 
+window.onload = init; 
+
+// Animates the map - zooms in
+function panHome(){
+    view.animate({
+        center: ourLoc, 
+        curation: 2000
+    }); 
+}
+
+
+//Creat a function that will go to a specific location
+//Using another API
+
+function panToLocation() {
+    var countryName = document.getElementById("country-name").value;
+
+    //Error check to make sure the country is spelled correctly 
+    if(countryName === "") {
+        alert("You didn't enter a country name!")
+    }
+
+    //we're accessing a REST API to get to the country's 
+    //location data 
+
+    var query = "https://restcountries.eu/rest/v2/name/"+countryName;
+
+    var lon = 0.0; 
+    var lat = 0.0; 
+    var location = ol.proj.fromLonLat([lon, lat])
+
+
+    view.animate({
+        center: location, 
+        duration: 2000
+    });
+}   
 
